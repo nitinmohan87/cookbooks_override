9d21ce83ba53333bf326fe1cea896843612cfcad
Nitin Mohan <nitin@rightscale.com>
2012-06-27 19:54:23 +0000

    [db_mysql] Fixed LICENSE file path

--- cookbooks/db_mysql/metadata.rb
+++ cookbooks/db_mysql/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "RightScale, Inc."
 maintainer_email "erik@rightscale.com"
-license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', 'LICENSE')))
 description      "Install customer my.cnf file"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
